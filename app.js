17bae1105f55c3e522c4b67f620b15c7d96e3658
sasha <caca-81.c@yandex.ru>
2024-04-17 21:45:54 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+// Базовые арифметические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3;
+console.log(volume);
+
+// Строки
+
+const city = 'Москва';
+const street = 'Новослабодская';
+console.log(city + ', ' + street + ' ' + 5);
